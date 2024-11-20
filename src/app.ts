1c0586e1d7f80d42269c67ca2843aacd48b20368
Azhar730 <azharmahmud730@gmail.com>
2024-11-20 22:30:00 +0600

    server done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.router';
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1/students', StudentRoutes)
+
+const getController = (req: Request, res: Response) => {
   res.send('Hello World!');
-});
+}
+
+app.get('/',getController );
 
 export default app;
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student = req.body.student;
+    const result = await StudentServices.createStudentIntoDB(student);
+    res.status(200).json({
+      success: true,
+      message: 'Student Created Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Students are Retrieve Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getSingleStudents = async (req: Request, res: Response) => {
+  try {
+    const {studentId} = req.params
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Students are Retrieve Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudents
+};
